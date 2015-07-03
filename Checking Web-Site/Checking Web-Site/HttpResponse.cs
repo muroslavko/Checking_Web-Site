d7e0885aa0f59b1ac0a7eaa18aee257f7fc1fcf7
muroslavko <myroslavdm@gmail.com>
2015-07-03 13:21:30 +0300

    add two ways of checking

---
+++ Checking Web-Site/Checking Web-Site/HttpResponse.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checking_Web_Site
+{
+    class HttpResponse : IResponse
+    {
+
+        public StringBuilder Test(string webSite, int attempts)
+        {
+            StringBuilder str = new StringBuilder();
+            WebRequest request = WebRequest.Create(webSite);
+            HttpWebResponse response;
+            try
+            {
+                for (int i = 0; i < attempts; i++)
+                {
+                    response = (HttpWebResponse)request.GetResponse();
+                    str.Append(String.Format("Ping {0}: {1}", webSite, response.StatusDescription));
+                }
+                //_print.Print(String.Format("Ping {0}: {1}", _webSite, Reply.Status));
+            }
+            catch (Exception e)
+            {
+                str.Append(e.Message);
+            }
+            return str;
+        }
+    }
+}
---
+++ Checking Web-Site/Checking Web-Site/PingRespose.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checking_Web_Site
+{
+    class PingRespose : IResponse
+    {
+        private Ping _pinger;
+
+        public StringBuilder Test(string webSite, int attempts)
+        {
+            StringBuilder str = new StringBuilder();
+            try
+            {
+                for (int i = 0; i < attempts; i++)
+                {
+                    PingReply Reply = _pinger.Send(webSite);
+                    str.Append(String.Format("Ping {0}: {1}", webSite, Reply.Status));
+                }
+                //_print.Print(String.Format("Ping {0}: {1}", _webSite, Reply.Status));
+            }
+            catch (Exception e)
+            {
+                str.Append(e.Message);
+            }
+            return str;
+        }
+    }
+}
